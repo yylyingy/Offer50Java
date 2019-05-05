248d986d77fc6426b3b8aeff0cac50950f3bf4b7
yylyingy <yylyingy@sina.com>
2019-05-05 19:20:59 +0800

    first commit

---
+++ src/main/java/io/github/yylyingy/YylyingyApplication.java
@@ -0,0 +1,22 @@
+package io.github.yylyingy;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class YylyingyApplication {
+
+//	static {
+//		System.setProperty("http.proxyHost", "localhost");
+//		System.setProperty("http.proxyPort", "8888");
+//	}
+
+	public static void main(String[] args)  {
+
+	}
+
+
+}
+
