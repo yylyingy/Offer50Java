df3b045586c038491c781f193125306811fd6023
yylyingy <yylyingy@sina.com>
2019-05-08 00:08:12 +0800

    update sixth

--- src/main/java/io/github/yylyingy/offer/Fifth.java
+++ src/main/java/io/github/yylyingy/offer/Fifth.java
@@ -9,7 +9,8 @@ import java.util.Stack;
  * <br> Author:      Yangyl
  * <br> Date:        2019/5/7 20:03
  */
-
+//https://www.nowcoder.com/practice/54275ddae22f475981afa2244dd448c6?tpId=13&tqId=11158&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking
+    //用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
 class Fifth {
     public static void main(String[] args) {
         push(1);
