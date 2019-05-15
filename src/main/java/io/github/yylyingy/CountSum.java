6444e471e5ef3e06101ca8220e5e85494b51a66e
yylyingy <yylyingy@sina.com>
2019-05-16 00:05:51 +0800

    update 查找数组内有多少对数之和等于指定的数

---
+++ src/main/java/io/github/yylyingy/CountSum.java
@@ -0,0 +1,59 @@
+package io.github.yylyingy;
+
+import java.util.HashMap;
+
+/**
+ * <br> ClassName:
+ * <br> Description: todo(这里用一句话描述这个类的作用)
+ * <br>
+ * <br> Author:      Yangyl
+ * <br> Date:        2019/5/15 21:35
+ */
+
+//计算数组里面有多少对数之和等于指定的数，数组元素可以相同，组成过一对的index不能再次组对
+class CountSum {
+    public static void main(String[] args) {
+//        int[] items = {1, 5, 1, 1, 99, 1, 99, 99,99};
+//        {1,1,99}
+//        {1,1,99,99}
+        int[] items ={5,8,1,9,10,99,2,3,98,98};
+        System.out.println(countSum(items, 100));
+
+    }
+
+    static int countSum(int[] items, int sum) {
+        //key item元素，value 元素的index
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < items.length; i++) {
+            int tmp = sum - items[i];
+            if (hashMap.containsKey(tmp)) {
+                hashMap.replace(tmp, hashMap.get(tmp) + 1);
+            } else {
+                hashMap.put(tmp, 1);
+            }
+            //只有一个数时继续
+            if (i == 0) {
+                continue;
+            }
+            Integer integer = hashMap.get(items[i]);
+            if ((integer) != null) {
+                    removeOrMinus(hashMap,items[i],integer);
+                    removeOrMinus(hashMap,sum - items[i],integer);
+                    count++;
+            }
+        }
+        return count;
+    }
+
+
+    static void removeOrMinus(HashMap<Integer, Integer> hashMap, Integer key, Integer value) {
+        if (value != null) {
+            if (!value.equals(1)) {
+                hashMap.replace(key, value - 1);
+            } else {
+                hashMap.remove(key);
+            }
+        }
+    }
+}
